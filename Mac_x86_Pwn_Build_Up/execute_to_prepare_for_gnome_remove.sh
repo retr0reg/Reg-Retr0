6f156e9cfc8d9ce8c7f2a4937fc969721318c9cf
DDizzzy79 <patrick_0429@163.com>
2023-01-30 22:10:34 +0800

    Updating file required for Installation

---
+++ Mac_x86_Pwn_Build_Up/execute_to_prepare_for_gnome_remove.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+gre='\033[34m'
+blu='\033[36m'
+end='\033[0m'
+echo -e "${gre}[*]${end} This setp wil update grub....."
+# sudo update-grub
+# sudo systemctl set-default multi-user.target
+echo -e "${gre}[+]${end} Now Reboot is required, please run: "sudo reboot" command by yourself"
---
+++ Mac_x86_Pwn_Build_Up/execute_to_remove_gnome.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+gre='\033[34m'
+blu='\033[36m'
+end='\033[0m'
+
+echo -e "${blu}[!] Those command required you to update apt, have you done this yet?"
+echo -e "${gre}y = Yes, n = No  (y/n): ${end}\c"
+read choice1
+
+if test $choice1 = 'n'
+then
+    echo -e "${blu}Updating Apt.......${end}"
+    # sudo apt update
+elif test $choice1 = 'y'
+    echo -e "${blu}Continuing...${end} "
+then
+echo -e "\n${blu}Now we run will run commands that will remove your gnome (desktop envoirment) \n${gre}Are You sure? y = Yes, n = No  (y/n):${end}${end} \c"
+read choice2
+    if test $choice2 = 'y'
+    then
+        # sudo apt remove gnome-shell
+        # sudo apt remove gnome
+        # sudo apt autoremove
+        # sudo apt purge gnome
+        # sudo snap remove store
+        # sudo snap remove gnome*
+        # sudo apt remove snapd
+        # sudo apt autoclean
+        # sudo apt clean
+        echo "finished."
+    else
+        exit
+    fi
+else
+    exit
+fi
