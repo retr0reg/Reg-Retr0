4f2dac51645f26d40abe255466764be6a1c48c77
DDizzzy79 <patrick_0429@163.com>
2023-02-02 10:07:44 +0800

    🔥 Added cool stuff

--- Mac_x86_Pwn_Build_Up/execute_to_change_apt_source.sh
+++ Mac_x86_Pwn_Build_Up/execute_to_change_apt_source.sh
@@ -4,6 +4,7 @@
 gre='\033[34m'
 blu='\033[36m'
 yel='\033[33m'
+<<<<<<< HEAD
 red='\033[31m'
 end='\033[0m'
 
@@ -15,6 +16,16 @@ echo -e "${gre}y = Yes, n = No  (y/N): ${end}\c"
 read choice1
 
 if test $choice1 = 'y' && test $choice0 = 'y' 
+=======
+end='\033[0m'
+
+
+echo -e "${blu}[!] Those command required you to change Apt source, You sure to do this?"
+echo -e "${gre}y = Yes, n = No  (y/n): ${end}\c"
+read choice1
+
+if test $choice1 = 'y'
+>>>>>>> 🔥 Added cool stuff
 then
     echo -e "${blu}[*]Backup Apt Source.....${end}"
     sudo cp /etc/apt/sources.list /etc/apt/sources_copy.list
@@ -22,8 +33,13 @@ then
     sudo echo "" > /etc/apt/sources.list
     APT_LIST=`cat apt_source.list`
     echo -e "${blu}[*]Changing Apt List.....${end}"
+<<<<<<< HEAD
     sudo echo "${APT_LIST}" > /etc/apt/sources.list
     echo -e "${blu}[*]Success!!!!${end}"
+=======
+    sudo echo "${APT_LIST}" > etc/apt/sources.list
+    echo -e "${blu}[*]Apt update Success!!!!${end}"
+>>>>>>> 🔥 Added cool stuff
 else
     echo -e "${yel}[-]FAILED!!!!${end}"
     exit
